6c24d7fbcb117df7a3810696c34e946b6c9de409
Qizman5 <rotar.andrii@chnu.edu.ua>
2026-09-21 16:41:38 +0300

    Fix XAML encoding, resolve binding/constructor errors, finalize MVVM UI navigation

--- Client/ClientApp/Platforms/Windows/App.xaml.cs
+++ Client/ClientApp/Platforms/Windows/App.xaml.cs
@@ -1,12 +1,11 @@
-namespace ClientApp.WinUI
-{
-    public partial class App : MauiWinUIApplication
-    {
-        public App()
-        {
-            this.InitializeComponent();
-        }
+namespace ClientApp.WinUI;
 
-        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+public partial class App : MauiWinUIApplication
+{
+    public App()
+    {
+        this.InitializeComponent();
     }
+
+    protected override MauiApp CreateMauiApp() => ClientApp.MauiProgram.CreateMauiApp();
 }
