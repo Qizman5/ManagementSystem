764b3db147d51af3ff54d0f902d239b430e4d1d3
Qizman5 <rotar.andrii@chnu.edu.ua>
2026-09-24 10:52:37 +0300

    Fix workflow to build automatically without hardcoded solution path

--- Server/ServerApp/ServerApp.bat
+++
@@ -1,8 +0,0 @@
-﻿@echo off
-chcp 65001 > nul
-title WMS Server
-echo ====================================
-echo      ЗАПУСК СЕРВЕРА (ServerApp)
-echo ====================================
-dotnet run --project "ServerApp/ServerApp.csproj"
-pause
